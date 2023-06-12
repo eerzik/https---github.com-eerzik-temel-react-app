45c527391961c7226633b2bfc6633e457ff5f5af
Engin ERZİK <erzik.engin@gmail.com>
2023-06-12 14:07:11 +0300

    Click event i

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,29 @@
 const Home = () => {
-    return (
-        <h2>Ana Sayfa</h2>
+    const handleClick = () => {
 
+        console.log('merhaba');
+    }
+
+    const handleClick2 = (isim) => {
+        console.log(isim);
+    }
+    const handleClick3 = (isim,e) => {
+        //console.log(isim,e.target);
+         console.log(isim,e);
+    }
+
+    return (
+        <div>
+            <h2>Ana Sayfa</h2>
+            {/*() pametre göndermiyorsak console e bir defa açıldığında yazdırır sonrasında bir işlem yapmaz
+            <button onClick={handleClick()} >Tıkla</button> */}
+            <button onClick={handleClick} >Tıkla</button>
+            <button onClick={() => handleClick2('luffy')} >Tıkla2</button>
+            <button onClick={(e) => handleClick3('luffy',e)} >Tıkla3</button>
+            <button onClick={(e) => {
+                console.log(e)
+            }} >Tıkla4</button>
+        </div>
     );
 }
 
