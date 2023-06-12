4da1f543261e996b873b7c163dcb0938737517f7
Engin ERZİK <erzik.engin@gmail.com>
2023-06-12 13:32:25 +0300

    Birden fazla Component Kulanımı

---
+++ src/Navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>AOS Blog</h1>
+            <div className="links">
+                <a href="/" >AnaSayfa</a>
+                <a href="/create" >Yeni Yazı</a>
+            </div>
+        </nav>
+
+    );
+}
+
+export default Navbar;
