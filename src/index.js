f052396187eccdbb999ac1265f19e51cd9804ab5
Engin ERZİK <erzik.engin@gmail.com>
2023-06-12 17:27:41 +0300

    UseEffect 2 defa render etmesi durumu not!!!

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //React.StrictMode 2 kez render ediyor (Geliştirme esnasında).
+  // Uygulamanız React 18'e güncelledikten sonra garip davranıyorsa, 
+  //varsayılan davranış 2 kez useEffect çalıştırılacak şekilde değiştirildi .
   <React.StrictMode>
     <App />
   </React.StrictMode>
