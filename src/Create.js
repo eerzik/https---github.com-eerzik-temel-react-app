9332e18fad12553396b2a1522707a6020ad6f167
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 12:56:07 +0300

    Verileri Post Etme İşlemi

--- src/Create.js
+++ src/Create.js
@@ -1,37 +1,45 @@
 import { useState } from "react";
+import { json } from "react-router-dom";
 
 
 const Create = () => {
 
-    const [baslik,setBaslik]=useState('');
-    const [aciklama,setAciklama]=useState('');
-    const [yazar,setYazar]=useState('luffy');
-
-    const handleSubmit =(e)=>{
+    const [baslik, setBaslik] = useState('');
+    const [aciklama, setAciklama] = useState('');
+    const [yazar, setYazar] = useState('luffy');
+    const [yukleniyor, setYukleniyor] = useState(false);
+    const handleSubmit = (e) => {
+        setYukleniyor(true);
         //preventDefault-Postback özelliği yok edildi.yoksa sayfa postback oluyor herşey temizleniyor.
         e.preventDefault();
-        const yazi={baslik,aciklama,yazar};
-        console.log(yazi);
+        const yazi = { ad: baslik, aciklama, yazar };
+        fetch('http://localhost:8000/yazilar/', {
+            method: 'POST',
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(yazi)
+        }).then(() => {
+            setYukleniyor(false);
+        })
     }
-    return ( 
+    return (
         <div className="create">
-            <h2 style={{color:'#ff793f'}} >Yeni Yazı Ekle</h2>
+            <h2 style={{ color: '#ff793f' }} >Yeni Yazı Ekle</h2>
             <form onSubmit={handleSubmit}>
                 <label>Yazı Başlık:</label>
-                <input type="text" required value={baslik} onChange={(e)=>setBaslik(e.target.value)} ></input>
+                <input type="text" required value={baslik} onChange={(e) => setBaslik(e.target.value)} ></input>
                 <label>Yazı Açıklama:</label>
-                <textarea required value={aciklama} onChange={(e)=>setAciklama(e.target.value)} ></textarea>
+                <textarea required value={aciklama} onChange={(e) => setAciklama(e.target.value)} ></textarea>
                 <label>Yazar:</label>
-                <select value={yazar} onChange={(e)=>setYazar(e.target.value)}>
+                <select value={yazar} onChange={(e) => setYazar(e.target.value)}>
                     <option value="luffy">Luffy</option>
                     <option value="zoro">Zoro</option>
                     <option value="sanji">Sanji</option>
                 </select>
-                <button>Ekle</button>
-              
+                {!yukleniyor&&<button>Ekle</button>}
+                {yukleniyor&&<button disabled>Yükleniyor</button>}
             </form>
         </div>
-     );
+    );
 }
- 
+
 export default Create;
