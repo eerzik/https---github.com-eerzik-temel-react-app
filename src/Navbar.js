8f4de413f912dda1726e8353196722e7c970ce38
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 11:19:03 +0300

    Router link oluşturma

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,19 @@
+
+import { Link } from "react-router-dom";
+
+
+
 const Navbar = () => {
     return (
         <nav className="navbar">
             <h1>AOS Blog</h1>
             <div className="links">
-                <a href="/" >AnaSayfa</a>
-                <a href="/create" style={{
-                    color: 'white',
-                    backgroundColor: '#ff793f',
-                    borderRadius: '8px'
-                }} >Yeni Yazı</a>
+              <Link to='/'>Anasayfa</Link>
+              <Link to='/create' style={{
+                color:'white',
+                backgroundColor:'#ff793f',
+                borderRadius:'8px'
+              }} >Yeni Yazı</Link>
             </div>
         </nav>
 
