94f436aa511f9be91cf1fa58cf8343dc483d61ed
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 11:51:20 +0300

    Yeni yazı ekleme sayfasını oluşturma

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,23 @@
+
+
+
 const Create = () => {
     return ( 
         <div className="create">
             <h2 style={{color:'#ff793f'}} >Yeni Yazı Ekle</h2>
+            <form>
+                <label>Yazı Başlık:</label>
+                <input type="text" required ></input>
+                <label>Yazı Açıklama:</label>
+                <textarea required></textarea>
+                <label>Yazar:</label>
+                <select>
+                    <option value="luffy">Luffy</option>
+                    <option value="zoro">Zoro</option>
+                    <option value="sanji">Sanji</option>
+                </select>
+                <button>Ekle</button>
+            </form>
         </div>
      );
 }
