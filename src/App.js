45f0d54216837b34a10631c8d8dfc3c553d42bff
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 10:58:22 +0300

    Router Oluşturma İşlemi npm install react-router-dom Switch yok artık Routes var

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import Navbar from "./Navbar";
 import AnaSayfa from "./Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
 
-
+//react-router-dom 6 ve sonrasında Switch yerine Routes geldi.
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <AnaSayfa></AnaSayfa>
+    <Router>
+      <div className="App">
+
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route path="/" element={<AnaSayfa />} />
+          </Routes>
+        </div>
+
       </div>
-    </div>
+    </Router>
   );
 }
 
