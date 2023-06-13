1f2624684c4876ded6b85887efc0cd7d1ef618ad
Engin ERZİK <erzik.engin@gmail.com>
2023-06-13 10:25:37 +0300

    Hata Yakalama İşlemi

--- src/Home.js
+++ src/Home.js
@@ -9,6 +9,8 @@ const Home = () => {
 
     const[yukleniyor,setYukleniyor]=useState(true);
 
+    const [hata,setHata] = useState(null);
+
     const handleClick = (id) => {
         const newBlogs=blogs.filter(blog=>blog.id!==id);
         setBlogs(newBlogs);
@@ -17,12 +19,16 @@ const Home = () => {
     useEffect(()=>{
       fetch('http://localhost:8000/yazilar')
       .then(res=>{
+        if(!res.ok) throw (Error('Veriler çekilemedi!'))
         return res.json();
       })
       .then(data=>{
         //console.log(data);
         setBlogs(data);
         setYukleniyor(false);
+      }).catch(err=>{
+        setYukleniyor(false);
+        setHata(err.message);
       })
     },[])
 
@@ -31,6 +37,7 @@ const Home = () => {
            
 
             <div className="home">
+                {hata && <div className="error" >{hata}</div>}
                 {yukleniyor && <div className="loading">Yükleniyor....</div>}
                {blogs &&  <BlogList bloglar={blogs} baslik="Bütün Yazılar" handleClick={handleClick} />}
              
